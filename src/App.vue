852d377962224110147a1e256f0da3da0b08d168
Edouard <edoaurd.hashimoto@edu.univ-fcomte.fr>
2025-11-25 10:48:53 +0100

    Création boutons

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,57 @@
 import { RouterLink, RouterView } from 'vue-router'
 import MyHeader from './components/MyHeader.vue'
 import DefaultLayout from './layouts/DefaultLayout.vue'
+import MyButton from './components/MyButton.vue'
+import MyTitle from './components/MyTitle.vue'
 </script>
 
 <template>
   <DefaultLayout>
     <template #header>
       <MyHeader />
+
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+
+      <div>
+        <h1 class="titreP">Welcome to My Website</h1>
+
+        <!-- Boutons -->
+        <MyButton>Default</MyButton>
+        <MyButton variant="outline">Outline</MyButton>
+        <MyButton class="-secondary">Secondary</MyButton>
+        <MyButton class="-danger">Danger</MyButton>
+        <MyButton class="-ghost">Ghost</MyButton>
+        <MyButton size="small">Small</MyButton>
+      </div>
+
+      <div>
+        <h1 class="titreP">Titre principal</h1>
+
+        <MyTitle as="h2" size="subtitle">Titre Small</MyTitle>
+
+        <MyTitle as="h1" size="texte2">Titre XL</MyTitle>
+
+        <MyTitle as="h3" size="texte">Titre Medium</MyTitle>
+
+        <MyTitle as="h2" class="-light">Titre Light</MyTitle>
+      </div>
     </template>
+
     <RouterView />
-    <template #aside>
-      <p>Aside</p>
-    </template>
-    <template #footer>
-      <p>Footer</p>
-    </template>
+
+    <template #aside><p>Aside</p></template>
+    <template #footer><p>Footer</p></template>
+
     <p>paragraphe</p>
   </DefaultLayout>
 </template>
+
+<style scoped>
+nav {
+  display: flex;
+  gap: 1rem;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './assets/main.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/main.scss'
 
 const app = createApp(App)
 
